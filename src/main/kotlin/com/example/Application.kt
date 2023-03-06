40c365cfc5f8510cb8e658726a1d9d7dd46c19b1
kotaro-yamada <kotaro-yamada@bm-sms.co.jp>
2023-03-06 19:38:46 +0900

    依存ごっそり削除

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -8,5 +8,4 @@ fun main(args: Array<String>): Unit =
 
 @Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
 fun Application.module() {
-    configureRouting()
 }
