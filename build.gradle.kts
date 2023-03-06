a75e4e5b04e0ac7ef6dfd04e660c6e6569373b43
kotaro-yamada <kotaro-yamada@bm-sms.co.jp>
2023-03-06 18:50:27 +0900

    add: 依存追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -66,4 +66,5 @@ dependencies {
     implementation("org.slf4j:slf4j-api:2.0.6")
     implementation("ch.qos.logback:logback-classic:1.4.5")
     implementation("io.github.microutils:kotlin-logging-jvm:3.0.5")
+    implementation("io.ktor:ktor-client-cio-jvm:2.2.4")
 }
